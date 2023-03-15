3cff1a05a40fe403df5a25cb06c74aaf626acf49
Aman <amank6209@gmail.com>
2023-03-15 08:07:27 +0530

    Initial commit

--- src/components/Food.js
+++ src/components/Food.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 export default (props) => {
 
- 
+  const style = {
+    left: `${props.dot[0]}%`,
+    top: `${props.dot[1]}%`
+  };
+
 
   return (
     <div className="snake-food"></div>
